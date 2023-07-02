46512318c08f0a626e84ba8a95c471f33a74faaa
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2023-07-02 21:56:46 +0200

    login page added

--- src/login.js
+++ src/login.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./login.css"
-
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./login.css";
+import { Container, Form } from "react-bootstrap";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [form, setform] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault ();
+    console.log(form);
+    const dto = {...form};
+    await axios.post();
+  }
+
+  return (
+    <Container>
+      <h2>Login</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="username@gmail.com"
+            value={form.email}
+          />
+          <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={form.password}
+            id="inputPassword5"
+            aria-describedby="passwordHelpBlock"
+          />
+        </Form.Group>
+      </Form>
+    </Container>
+  );
+};
+
+export default Login;
