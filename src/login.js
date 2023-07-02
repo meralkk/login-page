2189f73795b9000de9dd4db5270973f6b5893807
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2023-07-02 21:16:02 +0200

    Initial commit

---
+++ src/login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./login.css"
+
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
