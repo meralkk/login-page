949ed01c33209d41de4c3c8286dbdb1bf6aa0623
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2023-07-04 21:30:51 +0200

    Added social media buttons and designed CSS

--- src/login.js
+++ src/login.js
@@ -4,7 +4,12 @@ import "./app.css";
 import { Button, Container, Form } from "react-bootstrap";
 import axios from "axios";
 import banner from "../src/img/banner.png";
+import bgimage from "../src/img/banner.png";
 import logo from "../src/img/favicon.svg";
+import google from "../src/img/icons/google.svg";
+import github from "../src/img/icons/github.svg";
+import facebook from "../src/img/icons/facebook.svg";
+
 
 const Login = () => {
   const [form, setform] = useState({
@@ -20,8 +25,13 @@ const Login = () => {
   };
 
   return (
+    <>
+    <div>
+    <img className="bgimage" src={bgimage} alt="banner" />
+  </div>
+
     <div className="login-page">
-      <div className="log col-sm-6 col-lg-4">
+      <div className="login-screen col-sm-6 col-lg-4">
         <Container>
           <div>
             <img className="logo" src={logo} alt="logo" />
@@ -29,7 +39,10 @@ const Login = () => {
           <h2>Login</h2>
           <br />
           <Form>
-            <Form.Group className="form-group mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group
+              className="form-group mb-3"
+              controlId="exampleForm.ControlInput1"
+            >
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
@@ -46,10 +59,40 @@ const Login = () => {
                 aria-describedby="passwordHelpBlock"
               />
             </Form.Group>
-            <p>Forgot Password?</p>
+            <p className="forgot">Forgot Password?</p>
           </Form>
           <br />
-          <Button className="col-12">Sign in</Button>{" "}
+          <Button className="sign-in col-12">Sign in</Button>
+          <br />
+          <p className="text">or continue with</p>
+          <div className="social-media fluid">
+            <Button
+              className="google col-4"
+              style={{ maxWidth: "calc(33.33% - 8px)" }}
+            >
+              {" "}
+              <img src={google} alt="Google" />{" "}
+            </Button>
+
+            <Button
+              className="github col-4"
+              style={{ maxWidth: "calc(33.33% - 8px)" }}
+            >
+              {" "}
+              <img src={github} alt="GitHub" />{" "}
+            </Button>
+
+            <Button
+              className="facebook col-4"
+              style={{ maxWidth: "calc(33.33% - 8px)" }}
+            >
+              {" "}
+              <img src={facebook} alt="Facebook" />{" "}
+            </Button>
+          </div>
+          <p className="text">
+            Don't have an account yet? <strong className="forgot">Register for free</strong>
+          </p>
         </Container>
       </div>
 
@@ -57,6 +100,7 @@ const Login = () => {
         <img className="banner" src={banner} alt="banner" />
       </div>
     </div>
+    </>
   );
 };
 
